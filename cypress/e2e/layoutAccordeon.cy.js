b90ae97d433b010cff3fbb536f87b782f7402a6c
Natalia Kolesnychenko <n.kolesnychenko@gmail.com>
2023-12-01 13:09:47 +0200

    accordeon test added

---
+++ cypress/e2e/layoutAccordeon.cy.js
@@ -0,0 +1,22 @@
+/// <reference types= "cypress" />
+
+describe('Layout Accordeon tests', ()=> {
+
+    it('Test first accordeon visibility', ()=> {
+        let text = 'A nebula is an interstellar cloud of dust,'
+        cy.visit('pages/layout/accordion'); 
+
+        cy.get('nb-accordion-item-body').first().as('firstAccordionBody')
+        
+        cy.get('@firstAccordionBody')
+            .contains(text)
+            .should('be.hidden'); 
+
+        cy.get('nb-card button').click()
+
+        cy.get('@firstAccordionBody')
+            .contains(text)
+            .should('be.visible'); 
+    })
+    
+})

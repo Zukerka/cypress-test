da1bb53a2f05f30d51089b718987146785634de2
Natalia Kolesnychenko <n.kolesnychenko@gmail.com>
2023-12-01 14:29:17 +0200

    updated color theme test with each method

--- cypress/e2e/colorTheme.cy.js
+++ cypress/e2e/colorTheme.cy.js
@@ -2,25 +2,43 @@
 
 describe('Test color theme change', ()=> {
     
-    it('Change color theme sequentially', ()=>{
-        cy.visit('/') 
+    // it('Change color theme sequentially', ()=>{
+    //     cy.visit('/') 
+
+    //     const colors = {
+    //         "Light": "rgb(255, 255, 255)",
+    //         "Dark": "rgb(34, 43, 69)",
+    //         "Cosmic": "rgb(50, 50, 89)",
+    //         "Corporate": "rgb(255, 255, 255)"
+    //        };
+
+    //     for (const color in colors) {
+            
+    //         cy.get('nb-select').click();
+    //         cy.get('nb-option').contains(color).click(); 
+
+    //         const expectedColor = colors[color];
+
+    //         cy.get('nb-layout-header nav').should('have.css', 'background-color', expectedColor)
+    //     }
+    // })
+
+    it('Change color theme sequentially', () => {
+        cy.visit('/');
 
         const colors = {
             "Light": "rgb(255, 255, 255)",
             "Dark": "rgb(34, 43, 69)",
             "Cosmic": "rgb(50, 50, 89)",
             "Corporate": "rgb(255, 255, 255)"
-           };
+        };
 
-        for (const color in colors) {
-            
+        cy.wrap(Object.entries(colors)).each(([color, expectedColor]) =>{
             cy.get('nb-select').click();
-            cy.get('nb-option').contains(color).click(); 
-
-            const expectedColor = colors[color];
-
+            cy.get('nb-option').contains(color).click();
             cy.get('nb-layout-header nav').should('have.css', 'background-color', expectedColor)
-        }
-    })
+        }) 
+
+    });
     
 })

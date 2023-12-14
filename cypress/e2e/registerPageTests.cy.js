afff4523833b329ee48fadcf32419f6fcf5775c7
Natalia Kolesnychenko <n.kolesnychenko@gmail.com>
2023-12-14 18:49:19 +0200

    only deleted

--- cypress/e2e/registerPageTests.cy.js
+++ cypress/e2e/registerPageTests.cy.js
@@ -51,7 +51,7 @@ describe('Register Page tests', () => {
             registerPage.rePasswordErrorText.should('contain', ' Password confirmation is required! ')
         })
 
-        it.only('Password and confirm password don\'t match', () => {
+        it('Password and confirm password don\'t match', () => {
             registerPage.nameField.type('test')
             registerPage.emailField.type('test@test.io')
             registerPage.passwordField.type('password')
@@ -60,7 +60,7 @@ describe('Register Page tests', () => {
         })
     })
 
-    context.only('Happy pass tests', () => {
+    context('Happy pass tests', () => {
         it('Successful register', ()=> {
             registerPage.register('Natalia', 'test@test.io', 'passsword', 'passsword')
             cy.location('pathname').should('eq', '/pages')
